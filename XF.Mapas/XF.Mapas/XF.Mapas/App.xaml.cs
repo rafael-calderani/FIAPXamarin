06a8835a1fe91952818c8aa161423bcef9634b40
rafael-calderani <rafael.calderani@gmail.com>
2017-11-29 22:46:57 -0200

    novo projeto mapas

---
+++ XF.Mapas/XF.Mapas/XF.Mapas/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace XF.Mapas {
+    public partial class App : Application {
+        public App() {
+            InitializeComponent();
+
+            //MainPage = new XF.Mapas.MainPage();
+        }
+
+        protected override void OnStart() {}
+
+        protected override void OnSleep() {}
+
+        protected override void OnResume() {}
+    }
+}
---
+++ XF.Mapas/XF.Mapas/XF.Mapas/App_Code/ListaMenu.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XF.Mapas.App_Code {
+    class ListaMenu {
+        public string Descricao { get; set; }
+        public string Icone { get; set; }
+        public Type TargetType { get; set; }
+    }
+}
